e4fbdae1a22e0de010be4d9e125c4413c81416c2
Melissa Stone <melissa@puppetlabs.com>
2013-10-03 16:55:06 -0700

    Add OSX support

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,8 +8,23 @@
 # Sample Usage:
 #  class { 'git': }
 #
-class git {
+class git (
+  $git_version = "1.8.3.2"
+){
+  if $operatingsystem == 'Darwin' {
+    $source = "https://git-osx-installer.googlecode.com/files/git-${git_version}-intel-universal-snow-leopard.dmg"
+    file { '/usr/local/bin':
+      ensure => directory,
+      require => Package['git'],
+    }
+    file { '/usr/local/bin/git':
+      ensure => link,
+      target => '/usr/local/git/bin/git',
+      require => File['/usr/local/bin'],
+    }
+  }
   package { 'git':
     ensure => installed,
+    source => $source,
   }
 }
